6c774ca6a89f8ece056ca137013a3ff4a47a4d3c
Blaize9931 <blaize.kenny@outlook.com>
2026-02-06 17:52:47 +0000

    declared add button input variable. tested function.

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//add button
+const add = document.getElementById("add");
+//input field
+const todoInput = document.getElementById("todo-input");
+
+//When i click button it adds text in the input field.
+add.addEventListener("click", myFunction);
+  
+
+function myFunction() {
+  
+}
